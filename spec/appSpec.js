4b29634369143a82d3006fa42f2fb9e3e04e27fe
steven.eklou <steven.eklou@student.vinci.be>
2023-10-12 17:36:03 +0200

    beginnin of the project

---
+++ spec/appSpec.js
@@ -0,0 +1,17 @@
+import {Berlinclock} from "../app";
+
+describe("yuezbvfouzbf",function(){
+    it("blblblblblb",function(){
+        //arrange
+
+        let time = 5
+
+        //act
+        let result = Berlinclock.minuteLine(time);
+
+        //assert
+        expect(result).toBe();
+
+    })
+});
+
